f4464686a53bf9299203cb77eafae20b60d7633a
Eneko96 <imr.aem2526@gmail.com>
2023-04-10 12:30:13 +0100

    create build

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,5 +4,36 @@ export default defineConfig({
   test: {
     files: ['test/**/*.mjs', 'test/**/*.js'],
     include: ['test/**/*.mjs', 'test/**/*.js']
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        index: 'rest.mjs',
+        test: 'test/index.mjs'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]',
+        format: 'es',
+        dir: 'dist',
+        sourcemap: true,
+        sourcemapExcludeSources: true,
+        sourcemapPathTransform: (relativePath) => {
+          return relativePath
+        },
+        manualChunks: (id) => {
+          if (id.includes('node_modules')) {
+            return 'vendor'
+          }
+        },
+        preserveEntrySignatures: 'strict',
+        exports: 'auto',
+        externalLiveBindings: false,
+        inlineDynamicImports: false,
+        preferConst: true,
+        strict: true
+      }
+    }
   }
 })
